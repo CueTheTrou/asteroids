bd20234d16af5066fdaf9a129835b8e384a8daa6
CueTheTrou <j673mq@gmail.com>
2025-03-24 20:50:08 -0700

    G: Added to constants.py and main.py  to create player that can move, Added groups for player class to organize code better and improve game loop

--- main.py
+++ main.py
@@ -13,17 +13,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2 ,y = SCREEN_HEIGHT / 2)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return
+        
+        updatable.update(dt)
             
         screen.fill("black")
-
-        player.draw(screen)
-
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
